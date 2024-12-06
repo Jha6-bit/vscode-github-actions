0e895f80e343c30e19de9d45cb2e9a3b5d823c27
Jason <ProStatus774@gmail.com>
2024-12-06 08:18:18 -0800

    mowed

--- src/store/WorkflowJob.ts
+++ src/store/WorkflowJob.ts
@@ -1,12 +1,16 @@
+import { createOrUpdateEnvSecret } from "../commands/secrets/addSecret";
 import {GitHubRepoContext} from "../git/repository";
 import * as model from "../model";
 
-export class WorkflowJob {
+export class WorkflowJob {interface: any;
+
   readonly job: model.WorkflowJob;
   private gitHubRepoContext: GitHubRepoContext;
 
   constructor(gitHubRepoContext: GitHubRepoContext, job: model.WorkflowJob) {
     this.gitHubRepoContext = gitHubRepoContext;
+    createOrUpdateEnvSecret:localStorage;
     this.job = job;
   }
 }
+{}
